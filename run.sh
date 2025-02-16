0aa7d2b9e887645a521dd45c14d7dd59301f2c5d
Aleksander Długosz <olekdlugi@gmail.com>
2025-02-16 13:18:23 +0100

    Fix run script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Loading the cmake library"
-java LoadLibrary.java "$(dirname "$0")/lib/libSDL3-cmake.dylib"
+java LoadLibrary.java "$PWD/lib/libSDL3-cmake.dylib"
 echo ""
 echo "Loading the zig library"
-java LoadLibrary.java "$(dirname "$0")/lib/libSDL3.dylib"
+java LoadLibrary.java "$PWD/lib/libSDL3.dylib"
